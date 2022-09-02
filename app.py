4f5061620fb2fffff917669aeb41e54659594314
Jason Vero <86158597+jasonvero@users.noreply.github.com>
2022-09-01 22:38:08 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# --- Project 3: Building a Website with Django
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Django Framework
+
+# A framework is a library of reusable modules (building blocks)
+# These modules provide functionality for common tasks
+
+# A framework also defines a structure for our applications
+# It tells us what folders and files we should have in our project
+
+# Executing in Terminal
+    # pip install django ==2.1
+
+    # pyshop-mosh> django-admin startproject pyshop .
+        # This program takes various arguments.
+        # 'startproject' arguement, with it create a project called pyshop in the current folder,
+        # which is what the period represents
+        # If you don't include the '.' period, it will create its own folder which will look repetitive
